4be9ee4815fb4725ad6e772a2f9ab0c4c7e94952
Kushaal Kankane <kushaalkankane12345@gmail.com>
2026-01-19 07:14:32 +0000

    [Kushaal] Add scroll bar

--- src/components/LatestArticle.tsx
+++ src/components/LatestArticle.tsx
@@ -2,6 +2,7 @@ import LatestArticleCard from "./LatestArticleCard";
 import { getBlogs, type Blog } from "../api/blogs.ts";
 import BlogCardSkeleton from "./BlogCardSkeleton.tsx";
 import { useQuery } from "@tanstack/react-query";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area.tsx";
 
 function LatestArticle() {
 	const { data: blogs, isLoading, isError, error } = useQuery<Blog[]>({
@@ -11,8 +12,7 @@ function LatestArticle() {
 
 	const latestBlogs: Blog[] = blogs?.slice().sort((a:Blog, b:Blog) => (
         new Date(b.date).getTime() - new Date(a.date).getTime()
-    ))
-    .slice(0,5) ?? [];
+    )) ?? []
 	
 
     if (isLoading) {
@@ -32,15 +32,21 @@ function LatestArticle() {
     }
 
 	return (
-		<div className='flex flex-col p-4 md:p-0 md:pl-4 md:mr-8'>
-			<div className='text-xl font-bold my-6'>Latest Articles</div>
-			<div className='flex flex-col gap-4'>
+        <div className="flex flex-col p-4 md:p-0 md:pl-4 md:mr-8 min-h-0">
+            <div className="text-xl font-bold my-6">Latest Articles</div>
+
+            <ScrollArea className="h-[100vh] pr-3">
+            <div className="flex flex-col gap-4">
                 {latestBlogs.map((blog) => (
-                    <LatestArticleCard key={blog.id} blog={blog}/>
+                <LatestArticleCard key={blog.id} blog={blog} />
                 ))}
-			</div>
-		</div>
-	);
+            </div>
+
+            <ScrollBar orientation="vertical" />
+            </ScrollArea>
+        </div>
+        );
+
 }
 
 export default LatestArticle;
